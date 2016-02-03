de33e49ccd66a7ade27bb10aed2bbf5be3107b83
Roger Spielmann <rspielmann@snowflake.ch>
2016-02-03 18:25:10 +0100

    adds 01

---
+++ Problem01/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rspielmann
+ * Date: 03.02.2016
+ * Time: 17:38
+ */
+
+$start = microtime(true);
+$sum = 0;
+
+for ($i = 3; $i < 1000; $i ++) {
+
+    if($i % 3 == 0 || $i % 5 == 0) {
+        //echo $i . PHP_EOL;
+        $sum += $i;
+    }
+}
+
+$end = microtime(true);
+
+echo 'result . ' . $sum;
+echo 'duration ' . ($start-$end) * 1000;
